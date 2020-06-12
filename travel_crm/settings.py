728d08e799a029ccc931917d0ce147794bd93a0f
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-12 11:50:48 +0545

    apps added, material admin settings

--- travel_crm/settings.py
+++ travel_crm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #django admin dashboard
     'material.admin',
     'material.admin.default',
     # 'django.contrib.admin',
@@ -39,8 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #custom apps
     'apps.accounts',
+    'apps.blog',
+    'apps.enquiry',
     'apps.packages',
+    'apps.payment',
+    'apps.reportings',
+
+    #third party apps
     'rest_framework',
 ]
 
@@ -125,3 +134,26 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+
+MATERIAL_ADMIN_SITE = {
+    # 'HEADER':  'My site header',  # Admin site header
+    # 'TITLE':  'Your site title',  # Admin site title
+    # 'FAVICON':  'path/to/favicon',  # Admin site favicon (path to static should be specified)
+    # 'MAIN_BG_COLOR':  'blue',  # Admin site main color, css color should be specified
+    # 'MAIN_HOVER_COLOR':  'green',  # Admin site main hover color, css color should be specified
+    # 'PROFILE_PICTURE':  'path/to/image',  # Admin site profile picture (path to static should be specified)
+    # 'PROFILE_BG':  'path/to/image',  # Admin site profile background (path to static should be specified)
+    # 'LOGIN_LOGO':  'path/to/image',  # Admin site logo on login page (path to static should be specified)
+    # 'LOGOUT_BG':  'path/to/image',  # Admin site background on login/logout pages (path to static should be specified)
+    # 'SHOW_THEMES':  True,  #  Show default admin themes button
+    # 'TRAY_REVERSE': True,  # Hide object-tools and additional-submit-line by default
+    # 'NAVBAR_REVERSE': True,  # Hide side navbar by default
+    'SHOW_COUNTS': True, # Show instances counts for each model
+    'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
+        'packages': 'spa',
+    },
+    'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
+        # 'travels': 'room', eg: does not work
+    }
+}
