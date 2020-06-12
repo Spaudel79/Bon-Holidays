920a733251b4fe08e9ad36376a974ec80a3fe591
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-12 11:51:07 +0545

    test package model

--- apps/packages/admin.py
+++ apps/packages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Travel
+from django.contrib.admin import ModelAdmin, register
 
 # Register your models here.
+
+@register(Travel)
+class TravelAdmin(ModelAdmin):
+    list_display = ('destination', 'duration', 'price', 'date_created')
+    icon_name = 'tram'
+
+
+admin.site.site_header = 'Travel CRM'
+
--- apps/packages/models.py
+++ apps/packages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Travel(models.Model):
+    destination = models.CharField(max_length=255)
+    duration = models.IntegerField(null=True)
+    no_of_persons = models.IntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.destination}:{self.duration}:{self.price}"
