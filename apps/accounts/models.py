d7973475c7f52a8987ce5001a2e2678d9354bd09
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-15 10:05:48 +0545

    multiple user type and role based model test

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
@@ -8,7 +11,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
         """Creating new user and saving the user."""
         if not email:
-            raise ValueError('User must have a valid email')
+            raise ValueError('Admin must have a valid email')
         user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save(using=self._db)
@@ -35,3 +38,42 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     object = UserManager()
     USERNAME_FIELD = 'email'
+
+class UserProfile(models.Model):
+
+    USER_TYPES = (
+        ('a', 'Admin'),
+        ('g', 'Agent'),
+        ('c', 'Customer'),
+    )
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                related_name='profile', on_delete=models.CASCADE)
+    avatar = models.ImageField(blank=True)
+    about = models.TextField(blank=True)
+    user_type = models.CharField(max_length=1, choices=USER_TYPES, default='g')
+
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+    def save(self, *args, **kwargs):
+        if self.id and self.avatar:
+            current_avatar = UserProfile.objects.get(pk=self.id).avatar
+            if current_avatar != self.avatar:
+                current_avatar.delete()
+        super(UserProfile, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        target = reverse('accounts:profile', args=[self.user.username])
+        return target
+
+    def is_admin(self):
+        return self.user_type == 'a'
+
+    def is_agent(self):
+        return self.user_type == 'g'
+
+    def is_customer(self):
+        return self.user_type=='c'
