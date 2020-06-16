012f6b99b6f75bfce80c3502c58255bc7b5dd42e
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-16 12:11:00 +0545

    added author foreign key and autocomplete

--- apps/blogs/admin.py
+++ apps/blogs/admin.py
@@ -8,11 +8,12 @@ from django.contrib.admin import ModelAdmin, register
 @register(BlogPost)
 class BlogPostAdmin(ModelAdmin):
 
+    autocomplete_fields = ['author']
     def edit(self, obj):
         return format_html('<a class="btn-btn" href="/admin/blogs/blogpost/{}/change/">Change</a>', obj.id)
 
     def delete(self, obj):
         return format_html('<a class="btn-btn" href="/admin/blogs/blogpost/{}/delete/">Delete</a>', obj.id)
 
-    list_display = ('name', 'html_stripped', 'edit', 'delete')
+    list_display = ('name', 'author','html_stripped', 'date_created','edit', 'delete')
     icon_name = 'assignment'
--- apps/blogs/models.py
+++ apps/blogs/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from apps.accounts.models import UserProfile
 from ckeditor_uploader.fields import RichTextUploadingField
 
 class BlogPost(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     content = RichTextUploadingField()
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
