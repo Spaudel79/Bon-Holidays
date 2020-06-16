195598e0efc0cc72f01d74d5a32157647e66ab30
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-16 11:45:01 +0545

    add change and delete button

--- apps/blogs/admin.py
+++ apps/blogs/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from .models import BlogPost
+from django.utils.html import format_html
 from django.contrib.admin import ModelAdmin, register
 
 # Register your models here.
 
 @register(BlogPost)
 class BlogPostAdmin(ModelAdmin):
-    list_display = ('name',)
-    # icon_name = ''
+
+    def edit(self, obj):
+        return format_html('<a class="btn-btn" href="/admin/blogs/blogpost/{}/change/">Change</a>', obj.id)
+
+    def delete(self, obj):
+        return format_html('<a class="btn-btn" href="/admin/blogs/blogpost/{}/delete/">Delete</a>', obj.id)
+
+    list_display = ('name', 'html_stripped', 'edit', 'delete')
+    icon_name = 'assignment'
