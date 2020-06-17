87fc8673d9983649671e411f69675a8ec5a640ad
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-17 12:40:46 +0545

    updating destination and packages attributes

--- apps/packages/models.py
+++ apps/packages/models.py
@@ -5,12 +5,12 @@ from imagekit.processors import ResizeToFill
 class Destination(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+    discount = models.CharField(max_length=255, default="5% OFF")
     image = models.ImageField(blank=True)
     thumbnail = ImageSpecField(source='image',
                                       processors=[ResizeToFill(100, 50)],
                                       format='JPEG',
                                       options={'quality': 60})
-    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -19,6 +19,10 @@ class Package(models.Model):
     destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
     package_name = models.CharField(max_length=255)
     price = models.IntegerField()
+    duration = models.IntegerField(default=5)
+    discount = models.CharField(max_length=255, default="15% OFF")
+    discounted_price = models.IntegerField(default=230)
+    savings = models.IntegerField(default=230)
     rating = models.IntegerField(choices=((1, 1),
                                           (2, 2),
                                           (3, 3),
