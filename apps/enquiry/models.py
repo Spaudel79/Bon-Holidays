49938691d375640f4a3cc5855f30728832cc6c4b
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-17 13:37:44 +0545

    add Contact and Feedback model

--- apps/enquiry/models.py
+++ apps/enquiry/models.py
@@ -3,7 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, default="795 Australia")
+    phone = models.CharField(max_length=255, default="808 - 808")
+    email = models.EmailField(default="hello@hello.com")
+    description = models.TextField(null=True, blank=True)
+    opened_till = models.CharField(max_length=255, default="Mondey to Sunday")
 
     def __str__(self):
-        return self.name
+        return self.address
+
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField(null=True)
+
+
