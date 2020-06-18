4adb31b43d761f550696bcaa8eac600bade311da
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-18 12:44:01 +0545

    commenting api-auth path

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,3 @@
-# # packages/urls.py
-
-
-# packages/urls.py
 from django.urls import include, path
 from rest_framework import routers
 from . import views
@@ -14,22 +10,5 @@ router.register(r'bookmundi-account', views.BookmundiAccountViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
-
-
-
-
-
-
-# from django.urls import include, path
-# from django.conf.urls import url
-# from . import views
-#
-# # Wire up our API using automatic URL routing.
-# # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-# ]
-#
-# app_name = 'accounts'
