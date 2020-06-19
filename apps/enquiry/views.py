acb4de65ea75970551d7016bf4253b5530217fbc
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-19 12:03:00 +0545

    contact and feedback viewset

--- apps/enquiry/views.py
+++ apps/enquiry/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from .models import  Contact, Feedback
+from .serializers import ContactSerializer, FeedbackSerializer
 
-# Create your views here.
+class ContactViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+class FeedbackViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
