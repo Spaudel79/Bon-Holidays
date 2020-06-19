3f2550d1e6d7db22f4a450577ffc7eb30f883d21
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-19 13:07:15 +0545

    search by destination_name and order by date created

--- apps/packages/views.py
+++ apps/packages/views.py
@@ -1,7 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, filters
 from .models import  Destination, Package
 from .serializers import PackageSerializer
 
-class PackageViewSet(viewsets.ReadOnlyModelViewSet):
+class PackageViewSet(generics.ListAPIView, generics.RetrieveAPIView, viewsets.GenericViewSet):
     queryset = Package.objects.all()
     serializer_class = PackageSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['destination__name','date_created']
+    ordering_fields = ['date_created']
