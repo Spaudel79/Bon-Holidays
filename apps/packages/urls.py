61f7a17d64bdf3c14eadff0379c28e47853c0abc
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-22 13:19:50 +0545

    urls for top-activities and top-attractions

--- apps/packages/urls.py
+++ apps/packages/urls.py
@@ -1,13 +1,13 @@
 # packages/urls.py
 from django.urls import include, path
 from rest_framework import routers
-from .views import PackageViewSet, DestinationViewSet, TopAttractionsSerializer, TopActivitiesSerializer
+from .views import PackageViewSet, DestinationViewSet, TopActtractionsViewset, TopActivitiesViewset
 
 router = routers.DefaultRouter()
 router.register(r'packages', PackageViewSet)
 router.register(r'destination', DestinationViewSet)
-router.register(r'top-attractions', TopAttractionsSerializer)
-router.register(r'top-activities', TopActivitiesSerializer)
+router.register(r'top-attractions', TopActtractionsViewset)
+router.register(r'top-activities', TopActivitiesViewset)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
