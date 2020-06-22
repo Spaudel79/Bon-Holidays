0a351bcf5f10e52a521b2a5f85476b64c9af9ad7
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-22 12:52:57 +0545

    model admin for TopActivities and TopAttractions

--- apps/packages/admin.py
+++ apps/packages/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Destination, Package
+from .models import Destination, Package, TopAttractions, TopActivities
 from django.contrib.admin import ModelAdmin, register
 from imagekit.admin import AdminThumbnail
 from django.utils.html import format_html
@@ -38,7 +38,34 @@ class PackageAdmin(ModelAdmin):
     readonly_fields = ['image_display']
     icon_name = 'layers'
 
+@register(TopAttractions)
+class TopAttractionsAdmin(ModelAdmin):
+    def edit(self, obj):
+        return format_html('<a class="btn-btn" href="/admin/packages/topattractions/{}/change/">Change</a>', obj.id)
+
+    def delete(self, obj):
+        return format_html('<a class="btn-btn" href="/admin/packages/topattractions/{}/delete/">Delete</a>', obj.id)
+
+    list_display = ('image_display','title',)
+    image_display = AdminThumbnail(image_field='thumbnail')
+    image_display.short_description = 'Image'
+    readonly_fields = ['image_display']
+    icon_name = 'layers'
+
+@register(TopActivities)
+class TopActivitiesAdmin(ModelAdmin):
+    def edit(self, obj):
+        return format_html('<a class="btn-btn" href="/admin/packages/topactivities/{}/change/">Change</a>', obj.id)
+
+    def delete(self, obj):
+        return format_html('<a class="btn-btn" href="/admin/packages/topactivities/{}/delete/">Delete</a>', obj.id)
+
+    list_display = ('image_display','title','description')
+    image_display = AdminThumbnail(image_field='thumbnail')
+    image_display.short_description = 'Image'
+    readonly_fields = ['image_display']
+    icon_name = 'layers'
+
 admin.site.register(Destination, DestinationAdmin)
 admin.site.register(Package, PackageAdmin)
 admin.site.site_header = 'Travel CRM'
-
