da4022d32067ba4f9b5d325d1a6ffb09876f536c
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-22 13:18:34 +0545

    viewsets for top-activities and top-attractions

--- apps/packages/views.py
+++ apps/packages/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, generics, filters
-from .models import  Destination, Package
-from .serializers import PackageSerializer, DestinationSerializer
+from .models import  Destination, Package, TopAttractions, TopActivities
+from .serializers import PackageSerializer, DestinationSerializer, TopActivitiesSerializer, TopAttractionsSerializer
 
 class DestinationViewSet(generics.ListAPIView, generics.RetrieveAPIView, viewsets.GenericViewSet):
     queryset = Destination.objects.all()
@@ -12,3 +12,11 @@ class PackageViewSet(generics.ListAPIView, generics.RetrieveAPIView, viewsets.Ge
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['destination__name','date_created']
     ordering_fields = ['date_created']
+
+class TopActtractionsViewset(generics.ListAPIView, generics.RetrieveAPIView, viewsets.GenericViewSet):
+    queryset = TopAttractions.objects.all()
+    serializer_class = TopAttractionsSerializer
+
+class TopActivitiesViewset(generics.ListAPIView, generics.RetrieveAPIView, viewsets.GenericViewSet):
+    queryset = TopActivities.objects.all()
+    serializer_class = TopActivitiesSerializer
