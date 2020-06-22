71195c6f8009df74c01423c3f3aa5fd1a4c6bf32
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-22 13:17:39 +0545

    serializers for top-activities and top-attractions

--- apps/packages/serializers.py
+++ apps/packages/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Package, Destination
+from .models import Package, Destination, TopActivities, TopAttractions
 
 class PackageSerializer(serializers.ModelSerializer):
     #destination name instead of foreigen key id
@@ -12,3 +12,14 @@ class DestinationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Destination
         fields = '__all__'
+        
+class TopActivitiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TopActivities
+        fields = '__all__'
+
+class TopAttractionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TopAttractions
+        fields = '__all__'
+
