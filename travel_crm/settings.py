5885a05b24af5d9a1da1addd517ad2769a285a8e
silencedsre <jamakatel.shree.krishna@gmail.com>
2020-06-24 14:33:36 +0545

    using mysql instead of sqlite

--- travel_crm/settings.py
+++ travel_crm/settings.py
@@ -110,10 +110,19 @@ WSGI_APPLICATION = 'travel_crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
