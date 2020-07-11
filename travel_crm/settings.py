e06a426fffaa79b2ce31ef151b21b628fb359806
Admean <admean@gmail.local>
2020-07-11 16:30:43 +0545

    fronthome: front pages

--- travel_crm/settings.py
+++ travel_crm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f)_sy=de0f7z*qs19&uodz1+vx@4i5**$xl=0hz&6qrrajqqqj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ 'travel-dev2.ap-southeast-1.elasticbeanstalk.com', 'crm.mountaintigernepal.com', '127.0.0.1']
 
