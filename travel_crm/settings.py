0ba94aba788e7afd9632a924cbc2fa18d09bddba
Saroj Paudel <sarojpaudel79@gmail.com>
2020-11-11 11:03:10 +0545

    countofobjects

--- travel_crm/settings.py
+++ travel_crm/settings.py
@@ -249,8 +249,8 @@ REST_FRAMEWORK = {
 
 
     #for pagination
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 6,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
     #for filter
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
