f879781b82482973bbfabdea48885475b42363d1
sarojpaudel79 <sarojpaudel79@gmail.com>
2020-12-29 16:34:35 +0545

    pagechanged

--- travel_crm/settings.py
+++ travel_crm/settings.py
@@ -268,7 +268,7 @@ REST_FRAMEWORK = {
 # 'DEFAULT_PERMISSION_CLASSES': (
 #         'rest_framework.permissions.IsAuthenticated', ),
     # for pagination
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 6,
     # for filter
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
