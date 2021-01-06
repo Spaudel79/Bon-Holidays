f399e7ff78847531782fa2dd58583163964a6f9a
sarojpaudel79 <sarojpaudel79@gmail.com>
2021-01-06 13:22:30 +0545

    topdest

--- apps/packages/views.py
+++ apps/packages/views.py
@@ -44,7 +44,7 @@ from django_filters import rest_framework as filters
 class DestinationFrontListAPIView(ListAPIView):
     # queryset = Destination.objects.all().order_by('?')[:4]
     # queryset = Destination.objects.all().order_by('-date_created')[:4]
-    queryset = Destination.objects.all()
+    queryset = Destination.objects.all().order_by('-date_created')
     serializer_class = DestinationFrontSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['top', ]
