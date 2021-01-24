3138d8ac34108a9d8ccc2a3674cc555baeacd8fe
sarojpaudel79 <sarojpaudel79@gmail.com>
2021-01-24 10:52:05 +0545

    merge for change in package admin

--- apps/packages/admin.py
+++ apps/packages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from apps.accounts.models import User, UserProfile
 from django.contrib.admin import ModelAdmin, register
 from imagekit.admin import AdminThumbnail
 from django.utils.html import format_html
@@ -7,6 +8,7 @@ from django.utils.html import format_html
 
 
 class DestinationAdmin(ModelAdmin):
+
 
     search_fields = ['name']
 
@@ -26,19 +28,36 @@ class PackageAdmin(ModelAdmin):
 
     autocomplete_fields = ['destination']
 
-    def edit(self, obj):
-        return format_html('<a class="btn-btn" href="/admin/packages/package/{}/change/">Change</a>', obj.id)
 
-    def delete(self, obj):
-        return format_html('<a class="btn-btn" href="/admin/packages/package/{}/delete/">Delete</a>', obj.id)
 
-    list_display = ('image_display','package_name',  'featured', 'price', 'discounted_price',
-                    'savings', 'fix_departure', 'rating',
-                     'date_created', 'edit', 'delete')
-    image_display = AdminThumbnail(image_field='thumbnail')
-    image_display.short_description = 'Image'
-    readonly_fields = ['image_display']
-    icon_name = 'explore'
+    def get_list_filter(self, request):
+
+        if request.user.is_superuser:
+            def edit(self, obj):
+                return format_html('<a class="btn-btn" href="/admin/packages/package/{}/change/">Change</a>', obj.id)
+
+            def delete(self, obj):
+                return format_html('<a class="btn-btn" href="/admin/packages/package/{}/delete/">Delete</a>', obj.id)
+            list_display = ('package_name',  'featured', 'price', 'discounted_price',
+                            'savings', 'fix_departure', 'rating',
+                             'date_created', 'edit', 'delete')
+            # image_display = AdminThumbnail(image_field='thumbnail')
+            # image_display.short_description = 'Image'
+            # readonly_fields = ['image_display']
+            # icon_name = 'explore'
+            return list_display
+        else:
+            def edit(self, obj):
+                return format_html('<a class="btn-btn" href="/admin/packages/package/{}/change/">Change</a>', obj.id)
+
+            def delete(self, obj):
+                return format_html('<a class="btn-btn" href="/admin/packages/package/{}/delete/">Delete</a>', obj.id)
+            list_display = ('delete')
+            # image_display = AdminThumbnail(image_field='thumbnail')
+            # image_display.short_description = 'Image'
+            # readonly_fields = ['image_display']
+            icon_name = 'explore'
+            return list_display
 
     # class Media:
     #     js = ('ckeditor.js','configuration-ckeditor.js')
