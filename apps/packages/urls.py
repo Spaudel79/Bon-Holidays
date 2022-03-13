307457c80a119a28dff121c3fa42bfe968d3ff90
Saroj Paudel <sarojpaudel79@gmail.com>
2022-03-13 15:36:21 +0545

    minor changes in url

--- apps/packages/urls.py
+++ apps/packages/urls.py
@@ -1,26 +1,10 @@
 # packages/urls.py
 from django.urls import include, path
-from rest_framework import routers
 
-# from .views import PackageViewSet, DestinationViewSet, TopActtractionsViewset, TopActivitiesViewset
 from . import views
 
-# router = routers.DefaultRouter()
-# router.register(r'packages', PackageViewSet)
-# router.register(r'destination', DestinationViewSet)
-# router.register(r'top-attractions', TopActtractionsViewset)
-# router.register(r'top-activities', TopActivitiesViewset)
-
-# # Wire up our API using automatic URL routing.
-# # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
 
 urlpatterns = [
-    # path('api/destinations', views.DestinationFrontListAPIView.as_view(), name='api-destinations'),
-    # path('api/traveldeals', views.DestinationFrontListAPIView.as_view(), name='api-traveldeals'),
     path(
         "api/destinations",
         views.DestinationFrontListAPIView.as_view(),
@@ -48,8 +32,6 @@ urlpatterns = [
         views.ReviewAPIView.as_view(),
         name="api-review-post",
     ),
-    # path('api/activities/', views.TopActivitiesListAPIView.as_view(), name='api-activities'),
-    # path('api/activities/<int:pk>', views.TopActivitiesDetailsListAPIView.as_view(), name='api-activites_details'),
     path(
         "api/newactivities",
         views.NewActivityListAPIView.as_view(),
